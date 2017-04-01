a09eef1c553c921355737543957952bcc8832365
Code Ass <aycabta@gmail.com>
2017-04-01 13:26:02 +0900

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'service_providers#index'
+  get '/service_providers/new', to: 'service_providers#new'
+  post '/service_providers/create', to: 'service_providers#create'
+  get '/service_providers/:id', to: 'service_providers#show', as: 'service_providers'
 end
