a906f204b6e1de63fdaa32e14b54902e152a3ef6
Code Ass <aycabta@gmail.com>
2017-04-01 21:16:38 +0900

    Use resources for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'service_providers#index'
-  get '/service_providers/new', to: 'service_providers#new'
-  post '/service_providers/create', to: 'service_providers#create'
-  get '/service_providers/:id', to: 'service_providers#show', as: 'service_providers'
+  resources :service_providers, only: [:new, :create, :show] do
+    resources :consumers, only: [:index, :create]
+  end
 end
