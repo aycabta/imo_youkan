45e1c5e481bf591e88f2d056343f0622e843b924
Code Ass <aycabta@gmail.com>
2017-04-03 23:48:34 +0900

    Add consumers#create

--- app/controllers/consumers_controller.rb
+++ app/controllers/consumers_controller.rb
@@ -1,2 +1,7 @@
 class ConsumersController < ApplicationController
+  def create
+    @sp = ServiceProvider.find(params[:service_provider_id])
+    @sp.consumers.create(name: params[:name])
+    redirect_to(service_provider_path(@sp))
+  end
 end
