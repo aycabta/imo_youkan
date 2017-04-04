d17f994ff1e71b3d57cc8162931df603082e8bb2
Code Ass <aycabta@gmail.com>
2017-04-04 19:39:37 +0900

    Add User#find_or_create_by_auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ApplicationRecord
   has_one :token
+
+  def self.find_or_create_by_auth(auth)
+    user = User.find_or_create_by(provider: auth['provider'], uid: auth['uid'])
+
+    user.nickname = auth['info']['nickname']
+    user.email = auth['info']['email']
+    user.name = auth['info']['name']
+    user.image_url = auth['info']['image']
+    user.token = auth['credentials']['token']
+
+    user.save
+    user
+  end
 end
