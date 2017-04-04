e63dc51de2b5c218ebfffd5e98c46fddec375ee7
Code Ass <aycabta@gmail.com>
2017-04-04 11:48:56 +0900

    Add authorize! to ApplicatinHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def current_user
     @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
   end
+
+  def authorize!
+    redirect_to root_path unless current_user
+  end
 end
