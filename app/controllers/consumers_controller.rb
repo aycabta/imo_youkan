8072157c9096d6245bf20130f757212a1c659980
Code Ass <aycabta@gmail.com>
2017-04-06 01:55:41 +0900

    Check current_user's SeerviceProvider for ConsumersController#create

--- app/controllers/consumers_controller.rb
+++ app/controllers/consumers_controller.rb
@@ -2,7 +2,7 @@ class ConsumersController < ApplicationController
   before_action :authorize!, only: [:create]
 
   def create
-    @sp = ServiceProvider.find(params[:service_provider_id])
+    @sp = ServiceProvider.includes(:users).find_by(id: params[:service_provider_id], users: { id: current_user.id })
     @sp.consumers.create(name: params[:name])
     redirect_to(service_provider_path(@sp))
   end
