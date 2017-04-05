7dc47f5b9c92e0efa747fce193913e2812df4be3
Code Ass <aycabta@gmail.com>
2017-04-05 14:46:25 +0900

    Add association of ServiceProvider and User

--- app/models/scope.rb
+++ app/models/scope.rb
@@ -1,3 +1,4 @@
 class Scope < ApplicationRecord
   belongs_to :service_provider
+  has_many :approved_token, :through => :token_scopes
 end
--- app/models/service_provider.rb
+++ app/models/service_provider.rb
@@ -1,4 +1,6 @@
 class ServiceProvider < ApplicationRecord
   has_many :consumers
   has_many :scopes
+  has_many :service_provider_users
+  has_many :users, :through => :service_provider_users
 end
