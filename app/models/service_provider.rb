4debbe6f4adbb3b6a6ac355caf46274f41edcdbb
Code Ass <aycabta@gmail.com>
2017-04-08 06:09:06 +0900

    Add #owners and #owner? to ServiceProvider

--- app/models/service_provider.rb
+++ app/models/service_provider.rb
@@ -3,6 +3,11 @@ class ServiceProvider < ApplicationRecord
   has_many :scopes
   has_many :service_provider_users
   has_many :users, :through => :service_provider_users
+  has_many :owners, -> { where(service_provider_users: {is_owner: true}) }, :through => :service_provider_users, :source => :user
+
+  def owner?(user)
+    !!ServiceProvider.includes(:service_provider_users).find_by(id: self.id, service_provider_users: { is_owner: true, user: user })
+  end
 
   def add_user(user)
     sp_user = ServiceProviderUser.new
