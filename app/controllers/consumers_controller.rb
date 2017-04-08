1939d39cb0e812ed0f27f92e945d7ddcde97f623
Code Ass <aycabta@gmail.com>
2017-04-09 00:42:34 +0900

    Add service_providers#update for add_redirect_uri

--- app/controllers/consumers_controller.rb
+++ app/controllers/consumers_controller.rb
@@ -6,4 +6,14 @@ class ConsumersController < ApplicationController
     @sp.consumers.create(name: params[:name], owner: current_user)
     redirect_to(service_provider_path(@sp))
   end
+
+  def update
+    @sp = ServiceProvider.includes(:users).find_by(id: params[:service_provider_id], users: { id: current_user.id })
+    consumer = Consumer.find(params[:id])
+    case params[:type]
+    when 'add_redirect_uri'
+      consumer.redirect_uris.create(uri: params[:redirect_uri])
+    end
+    redirect_to(service_provider_path(@sp))
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'service_providers#index'
   resources :service_providers, only: [:create, :show, :update] do
-    resources :consumers, only: [:index, :create]
+    resources :consumers, only: [:index, :create, :update]
   end
   post '/:service_provider_id/oauth2/token', to: 'oauth2#token', as: 'oauth2_token'
   get '/auth/:provider/callback', to: 'sessions#create', as: 'auth_callback'
