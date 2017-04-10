780f856c989bb8d6f24a9fc6d9772adfaed262e8
Code Ass <aycabta@gmail.com>
2017-04-11 00:07:49 +0900

    Allow GET on /oauth2/authorize

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :service_providers, only: [:create, :show, :update] do
     resources :consumers, only: [:index, :create, :update]
   end
-  post '/:service_provider_id/oauth2/token', to: 'oauth2#token', as: 'oauth2_token'
+  match '/:service_provider_id/oauth2/token', to: 'oauth2#token', as: 'oauth2_token', via: [:get, :post]
   get '/:service_provider_id/oauth2/authorize', to: 'oauth2#authorize', as: 'oauth2_authorize'
   post '/:service_provider_id/oauth2/authorize', to: 'oauth2#authorize_redirect_with_code', as: 'oauth2_authorize_redirect_with_code'
   get '/auth/:provider/callback', to: 'sessions#create', as: 'auth_callback'
