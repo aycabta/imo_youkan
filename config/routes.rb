e14327d9a4329c90baa68c60cf0da084e00a5bb9
Code Ass <aycabta@gmail.com>
2017-04-11 13:09:42 +0900

    Remove unnecessary comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'service_providers#index'
   resources :service_providers, only: [:create, :show, :update] do
     resources :consumers, only: [:index, :create, :update]
