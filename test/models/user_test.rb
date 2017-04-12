abbe6ec4cca199bdac44336ffbb56a5045166d1d
Code Ass <aycabta@gmail.com>
2017-04-12 13:52:20 +0900

    Add test for User#find_or_create_by_auth

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'User is built by #find_or_create_by_auth' do
+    user = User.find_or_create_by_auth({
+      'provider' => 'testprovider',
+      'uid' => '0123abc4567def89',
+      'info' => {
+        'nickname' => 'aycabta',
+        'email' => 'aycabta@gmail.com',
+        'name' => 'Code Ass',
+        'image' => 'http://hogehoge.com/image.png'
+      }
+    })
+    refute_nil(user)
+    assert_kind_of(String, user.name)
+  end
 end
