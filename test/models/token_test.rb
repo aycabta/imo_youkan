17c4115cce338c4d48aa7dbf1c724be3665814b9
Code Ass <aycabta@gmail.com>
2017-04-17 04:35:47 +0900

    Add test for #set_as_implicit

--- test/models/token_test.rb
+++ test/models/token_test.rb
@@ -9,4 +9,24 @@ class TokenTest < ActiveSupport::TestCase
     assert_kind_of(String, token.access_token)
     assert_nil(token.refresh_token)
   end
+
+  test '#set_as_implicit is well-behaved' do
+    scopes = %w(basic profile data)
+    redirect_uri = 'http://foo.com/'
+    sp = ServiceProvider.create!(name: 'a web service')
+    scopes.each { |name| sp.scopes.create!(name: name) }
+    consumer = Consumer.create!(name: 'a consumer', service_provider: sp, owner: User.create)
+    consumer.redirect_uris.create!(uri: redirect_uri)
+    token = Token.create!(grant: 'implicit', consumer: consumer)
+    token.save
+    token.set_as_implicit(scopes, 'teststate', redirect_uri)
+    assert_equal(token.grant, 'implicit')
+    assert_not_nil(token.access_token)
+    assert_kind_of(String, token.access_token)
+    assert_nil(token.refresh_token)
+    token.approved_scopes.each do |scope|
+      assert(scopes.include?(scope.name))
+    end
+    assert_equal(token.redirect_uri.uri, redirect_uri)
+  end
 end
