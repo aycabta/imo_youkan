bb6cb971486c78a5880d8def967854e471f558d4
Code Ass <aycabta@gmail.com>
2017-04-17 04:08:06 +0900

    Add test for Token#set_as_client_credentials

--- test/models/token_test.rb
+++ test/models/token_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class TokenTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#set_as_client_credentials is well-behaved' do
+    token = Token.new(consumer: Consumer.new)
+    token.set_as_client_credentials
+    assert_not_nil(token.access_token)
+    assert_kind_of(String, token.access_token)
+    assert_nil(token.refresh_token)
+  end
 end
