863b65c491b97737b581e3d12ef31830faa49f6f
Code Ass <aycabta@gmail.com>
2017-04-17 04:44:35 +0900

    Add commit for #set_as_implicit with partial scopes

--- test/models/token_test.rb
+++ test/models/token_test.rb
@@ -30,4 +30,25 @@ class TokenTest < ActiveSupport::TestCase
     end
     assert_equal(token.redirect_uri.uri, redirect_uri)
   end
+
+  test '#set_as_implicit with partial scopes is well-behaved' do
+    scopes = %w(basic profile data post)
+    partial_scopes = scopes[0, 2]
+    redirect_uri = 'http://foo.com/'
+    sp = ServiceProvider.create!(name: 'a web service')
+    scopes.each { |name| sp.scopes.create!(name: name) }
+    consumer = Consumer.create!(name: 'a consumer', service_provider: sp, owner: User.create)
+    consumer.redirect_uris.create!(uri: redirect_uri)
+    token = Token.new(consumer: consumer)
+    token.set_as_implicit(partial_scopes, 'teststate', redirect_uri)
+    assert_equal(token.grant, 'implicit')
+    assert_not_nil(token.access_token)
+    assert_kind_of(String, token.access_token)
+    assert_nil(token.refresh_token)
+    token.approved_scopes.each do |scope|
+      assert_not_nil(partial_scopes.delete(scope.name))
+    end
+    assert_empty(partial_scopes)
+    assert_equal(token.redirect_uri.uri, redirect_uri)
+  end
 end
