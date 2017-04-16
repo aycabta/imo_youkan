0112df7741ee60044697212ab6d137b7b11bf31f
Code Ass <aycabta@gmail.com>
2017-04-17 04:09:25 +0900

    Add assertion for Token#grant

--- test/models/token_test.rb
+++ test/models/token_test.rb
@@ -4,6 +4,7 @@ class TokenTest < ActiveSupport::TestCase
   test '#set_as_client_credentials is well-behaved' do
     token = Token.new(consumer: Consumer.new)
     token.set_as_client_credentials
+    assert_equal(token.grant, 'client_credentials')
     assert_not_nil(token.access_token)
     assert_kind_of(String, token.access_token)
     assert_nil(token.refresh_token)
