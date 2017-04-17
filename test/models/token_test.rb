a95fb1da829a9a52cce2b0d24ad836bedd58bc97
Code Ass <aycabta@gmail.com>
2017-04-17 14:42:17 +0900

    Add test for #set_tokens_for_authorization_code

--- test/models/token_test.rb
+++ test/models/token_test.rb
@@ -101,4 +101,27 @@ class TokenTest < ActiveSupport::TestCase
     assert_empty(partial_scopes)
     assert_equal(token.redirect_uri.uri, redirect_uri)
   end
+
+  test '#set_tokens_for_authorization_code is well-behaved' do
+    scopes = %w(basic profile data)
+    redirect_uri = 'http://foo.com/'
+    state = 'teststate'
+    sp = ServiceProvider.create!(name: 'a web service')
+    scopes.each { |name| sp.scopes.create!(name: name) }
+    consumer = Consumer.create!(name: 'a consumer', service_provider: sp, owner: User.create)
+    consumer.redirect_uris.create!(uri: redirect_uri)
+    token = Token.new(consumer: consumer)
+    token.set_as_authorization_code(scopes, state, redirect_uri)
+    token.set_tokens_for_authorization_code
+    assert_equal(token.grant, 'authorization_code')
+    assert_equal(token.state, state)
+    assert_not_nil(token.code)
+    assert_kind_of(String, token.code)
+    assert_not_nil(token.access_token)
+    assert_not_nil(token.refresh_token)
+    token.approved_scopes.each do |scope|
+      assert(scopes.include?(scope.name))
+    end
+    assert_equal(token.redirect_uri.uri, redirect_uri)
+  end
 end
