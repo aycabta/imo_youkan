7f582ed83758fbb521501d2637f9cbaf83304ddc
Code Ass <aycabta@gmail.com>
2017-04-17 15:10:23 +0900

    Add assertion for Token#expires_in

--- test/models/token_test.rb
+++ test/models/token_test.rb
@@ -10,6 +10,7 @@ class TokenTest < ActiveSupport::TestCase
     assert_not_nil(token.access_token)
     assert_kind_of(String, token.access_token)
     assert_nil(token.refresh_token)
+    assert_kind_of(Time, token.expires_in)
   end
 
   test '#set_as_implicit is well-behaved' do
@@ -31,6 +32,7 @@ class TokenTest < ActiveSupport::TestCase
       assert(scopes.include?(scope.name))
     end
     assert_equal(token.redirect_uri.uri, redirect_uri)
+    assert_kind_of(Time, token.expires_in)
   end
 
   test '#set_as_implicit with partial scopes is well-behaved' do
@@ -54,6 +56,7 @@ class TokenTest < ActiveSupport::TestCase
     end
     assert_empty(partial_scopes)
     assert_equal(token.redirect_uri.uri, redirect_uri)
+    assert_kind_of(Time, token.expires_in)
   end
 
   test '#set_as_authorization_code is well-behaved' do
@@ -76,6 +79,7 @@ class TokenTest < ActiveSupport::TestCase
       assert(scopes.include?(scope.name))
     end
     assert_equal(token.redirect_uri.uri, redirect_uri)
+    assert_nil(token.expires_in)
   end
 
   test '#set_as_authorization_code with partial scopes is well-behaved' do
@@ -100,6 +104,7 @@ class TokenTest < ActiveSupport::TestCase
     end
     assert_empty(partial_scopes)
     assert_equal(token.redirect_uri.uri, redirect_uri)
+    assert_nil(token.expires_in)
   end
 
   test '#set_tokens_for_authorization_code is well-behaved' do
@@ -123,6 +128,7 @@ class TokenTest < ActiveSupport::TestCase
       assert(scopes.include?(scope.name))
     end
     assert_equal(token.redirect_uri.uri, redirect_uri)
+    assert_kind_of(Time, token.expires_in)
   end
 
   test '#set_tokens_for_authorization_code without code is ill-behaved' do
@@ -138,5 +144,6 @@ class TokenTest < ActiveSupport::TestCase
     assert_raise ActiveRecord::RecordInvalid do
       token.set_tokens_for_authorization_code
     end
+    assert_kind_of(Time, token.expires_in)
   end
 end
