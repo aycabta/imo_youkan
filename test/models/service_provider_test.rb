329ae307307bfc840b6eab71473a6a0481424cf7
Code Ass <aycabta@gmail.com>
2017-04-17 17:56:17 +0900

    Remove unnecessary reload

--- test/models/service_provider_test.rb
+++ test/models/service_provider_test.rb
@@ -7,8 +7,8 @@ class ServiceProviderTest < ActiveSupport::TestCase
     assert_equal(0, sp.owners.size)
     sp.add_user_as_owner(User.create!(name: 'the owner'))
     sp.reload
-    assert_equal(1, sp.reload.users.size)
-    assert_equal(1, sp.reload.owners.size)
+    assert_equal(1, sp.users.size)
+    assert_equal(1, sp.owners.size)
   end
 
   test '#add_owner is well-behaved' do
@@ -17,7 +17,7 @@ class ServiceProviderTest < ActiveSupport::TestCase
     assert_equal(0, sp.owners.size)
     sp.add_user(User.create!(name: 'a user'))
     sp.reload
-    assert_equal(1, sp.reload.users.size)
-    assert_equal(0, sp.reload.owners.size)
+    assert_equal(1, sp.users.size)
+    assert_equal(0, sp.owners.size)
   end
 end
