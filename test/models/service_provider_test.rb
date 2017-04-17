30e6985d8f6c4fdfce61d42de12f89ca72810eed
Code Ass <aycabta@gmail.com>
2017-04-17 17:47:27 +0900

    Add test for ServiceProvider#add_user_as_owner

--- test/models/service_provider_test.rb
+++ test/models/service_provider_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class ServiceProviderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#add_user_as_owner is well-behaved' do
+    sp = ServiceProvider.create!(name: 'a web service')
+    assert_equal(0, sp.users.size)
+    assert_equal(0, sp.owners.size)
+    sp.add_user_as_owner(User.create!(name: 'the owner'))
+    sp.reload
+    assert_equal(1, sp.reload.users.size)
+    assert_equal(1, sp.reload.owners.size)
+  end
 end
