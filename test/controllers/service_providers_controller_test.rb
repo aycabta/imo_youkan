b4ecbb96e61bd183c1335106516bbdcd15ae6615
Code Ass <aycabta@gmail.com>
2017-04-20 02:36:52 +0900

    Add test for root page

--- test/controllers/service_providers_controller_test.rb
+++ test/controllers/service_providers_controller_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class ServiceProvidersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should get root without login' do
+    get(root_path)
+    assert_response(:success)
+    assert_nil(assigns(:sps))
+    assert_not_nil(assigns(:new_sp))
+  end
 end
