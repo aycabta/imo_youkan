14430fc9921f9a819e4b2d23724fc65eefec374f
Code Ass <aycabta@gmail.com>
2017-04-30 12:31:50 +0900

    Add test for adding some scopes to ServiceProvider

--- test/controllers/service_providers_controller_test.rb
+++ test/controllers/service_providers_controller_test.rb
@@ -45,4 +45,27 @@ class ServiceProvidersControllerTest < ActionDispatch::IntegrationTest
     assert_equal(sp.scopes.size, 1)
     assert_equal(sp.scopes[0].name, 'profile')
   end
+
+  test 'should add some scopes to ServiceProvider' do
+    ldap_user = sign_in_as(:great_user)
+    post(service_providers_path, params: { service_provider: { name: 'test service provider' } })
+    user = User.find_by(uid: ldap_user.uid)
+    sp = ServiceProvider.find(assigns(:sp).id)
+    params = {
+      type: 'add_scope',
+      name: 'profile',
+      description: 'user description'
+    }
+    put(service_provider_path(sp.id), params: params)
+    assert_response(:found)
+    params = {
+      type: 'add_scope',
+      name: 'basic',
+      description: 'user basic information'
+    }
+    put(service_provider_path(sp.id), params: params)
+    assert_response(:found)
+    assert_equal(sp.scopes.size, 2)
+    assert_equal(sp.scopes.map(&:name).sort, %w{basic profile})
+  end
 end
