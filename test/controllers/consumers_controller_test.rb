28a93581309e8c78069c5df50b89393ab36d6d2a
Code Ass <aycabta@gmail.com>
2017-05-02 01:10:36 +0900

    Add test for consumers#show

--- test/controllers/consumers_controller_test.rb
+++ test/controllers/consumers_controller_test.rb
@@ -12,4 +12,11 @@ class ConsumersControllerTest < ActionDispatch::IntegrationTest
     assert_response(:found)
     assert_redirected_to(service_provider_path(@sp.id))
   end
+
+  test 'should show Consumer' do
+    post(service_provider_consumers_path(@sp.id), params: { name: 'test consumer' })
+    get(service_provider_consumer_path(@sp.id, Consumer.find_by(name: 'test consumer').id))
+    assert_response(:success)
+    assert_not_nil(assigns(:consumer))
+  end
 end
