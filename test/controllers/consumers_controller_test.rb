79ddd3d125e078d54674f12647b31b6d8cd147ac
Code Ass <aycabta@gmail.com>
2017-05-02 01:08:49 +0900

    Add test for consumers#create

--- test/controllers/consumers_controller_test.rb
+++ test/controllers/consumers_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class ConsumersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    sign_in_as(:great_user)
+    post(service_providers_path, params: { service_provider: { name: 'test service provider' } })
+    @sp = ServiceProvider.find(assigns(:sp).id)
+  end
+
+  test 'should create Consumer' do
+    post(service_provider_consumers_path(@sp.id), params: { name: 'test consumer' })
+    assert_response(:found)
+    assert_redirected_to(service_provider_path(@sp.id))
+  end
 end
