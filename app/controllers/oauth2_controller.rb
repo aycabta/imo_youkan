70d987732f4c58666af23f98aee39e82e3cba637
Code Ass <aycabta@gmail.com>
2017-05-05 14:39:38 +0900

    Remove functions what is in application controller now

--- app/controllers/oauth2_controller.rb
+++ app/controllers/oauth2_controller.rb
@@ -3,24 +3,6 @@ class OAuth2Controller < ApplicationController
   before_action :get_service_provider
   before_action :check_content_type, only: [:unauthorized, :revoke, :introspect], if: -> { request.post? }
 
-  private def get_service_provider
-    @sp = ServiceProvider.find(params[:service_provider_id])
-  end
-
-  private def check_content_type
-    unless request.content_type == 'application/x-www-form-urlencoded'
-      json = {
-        error: 'invalid_request',
-        error_description: 'Request header validation failed.',
-        error_details: {
-          content_type: "#{request.content_type} is invalid, must be application/x-www-form-urlencoded"
-        },
-        status: 'error'
-      }
-      render(json: json, status: :bad_request)
-    end
-  end
-
   def authorize
     if params[:response_type].nil?
       json = {
