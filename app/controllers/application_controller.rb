6fa09e2ed295a1dae5d4d34fbf06b81bc705518f
Code Ass <aycabta@gmail.com>
2017-05-05 13:44:03 +0900

    Move check_content_type and get_service_provideer to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,22 @@ class ApplicationController < ActionController::Base
   def authorize!
     redirect_to(root_path) unless current_user
   end
+
+  helper_method def check_content_type
+    unless request.content_type == 'application/x-www-form-urlencoded'
+      json = {
+        error: 'invalid_request',
+        error_description: 'Request header validation failed.',
+        error_details: {
+          content_type: "#{request.content_type} is invalid, must be application/x-www-form-urlencoded"
+        },
+        status: 'error'
+      }
+      render(json: json, status: :bad_request)
+    end
+  end
+
+  helper_method def get_service_provider
+    @sp = ServiceProvider.find(params[:service_provider_id])
+  end
 end
