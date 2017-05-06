982ee9006531e4691f73f63f288e41690f166c7a
Code Ass <aycabta@gmail.com>
2017-05-06 22:11:52 +0900

    Add abnormal test for unauthorized with invalid redirect_uri

--- test/controllers/oauth2_controller_test.rb
+++ test/controllers/oauth2_controller_test.rb
@@ -49,4 +49,21 @@ class OAuth2ControllerTest < ActionDispatch::IntegrationTest
     assert_equal("client_id (#{consumer.client_id_key + 'unknown'}) is unknown", json['error_description'])
     assert_equal('abcABC', json['state'])
   end
+
+  test 'should fail to /oauth2/unauthorize with unknown redirect_uri' do
+    sp = ServiceProvider.first
+    consumer = sp.consumers.first
+    redirect_uri = consumer.redirect_uris.first.uri + 'unknown'
+    params = {
+      client_id: consumer.client_id_key,
+      redirect_uri: redirect_uri,
+      state: 'abcABC'
+    }
+    post(oauth2_unauthorized_path(sp), params: params)
+    assert_response(:bad_request)
+    json = JSON.parse(response.body)
+    assert_equal('invalid_request', json['error'])
+    assert_equal("redirect_uri (#{redirect_uri}) is unknown", json['error_description'])
+    assert_equal('abcABC', json['state'])
+  end
 end
