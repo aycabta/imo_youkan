cc3e869c419c509dff95f561dd0cae5176c45190
Code Ass <aycabta@gmail.com>
2017-05-06 22:06:05 +0900

    Move test for /oauth2/token to sub-dir

--- test/controllers/oauth2/token_controller_test.rb
+++ test/controllers/oauth2/token_controller_test.rb
@@ -1,5 +1,14 @@
 require 'test_helper'
 
 class OAuth2::TokenControllerTest < ActionDispatch::IntegrationTest
+  test 'should fail /oauth2/token with invalid Content-Type' do
+    post(oauth2_token_path(ServiceProvider.first.id), headers: { 'CONTENT_TYPE': 'text/plain' })
+    assert_response(:bad_request)
+    json = JSON.parse(response.body)
+    assert_equal('invalid_request', json['error'])
+    assert_equal('Request header validation failed.', json['error_description'])
+    assert_equal('text/plain is invalid, must be application/x-www-form-urlencoded', json['error_details']['content_type'])
+    assert_equal('error', json['status'])
+  end
 end
 
--- test/controllers/oauth2_controller_test.rb
+++ test/controllers/oauth2_controller_test.rb
@@ -1,16 +1,6 @@
 require 'test_helper'
 
 class OAuth2ControllerTest < ActionDispatch::IntegrationTest
-  test 'should fail /oauth2/token with invalid Content-Type' do
-    post(oauth2_token_path(ServiceProvider.first.id), headers: { 'CONTENT_TYPE': 'text/plain' })
-    assert_response(:bad_request)
-    json = JSON.parse(response.body)
-    assert_equal('invalid_request', json['error'])
-    assert_equal('Request header validation failed.', json['error_description'])
-    assert_equal('text/plain is invalid, must be application/x-www-form-urlencoded', json['error_details']['content_type'])
-    assert_equal('error', json['status'])
-  end
-
   test 'should fail /oauth2/authorize without response_type' do
     get(oauth2_authorize_path(ServiceProvider.first.id))
     assert_response(:bad_request)
