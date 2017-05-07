44c7604fdcfeffdb20f1ce409bc99db1a022a661
Code Ass <aycabta@gmail.com>
2017-05-07 16:17:59 +0900

    Add test for /oauth2/token with authorization_code

--- test/controllers/oauth2/token_controller_test.rb
+++ test/controllers/oauth2/token_controller_test.rb
@@ -44,5 +44,35 @@ class OAuth2::TokenControllerTest < ActionDispatch::IntegrationTest
     assert_kind_of(Integer, json['expires_in'])
     assert_equal('abcABC', json['state'])
   end
+
+  test 'should get /oauth2/token with authorization_code' do
+    ldap_user = sign_in_as(:great_user)
+    sp = ServiceProvider.all.max{ |a, b| a.scopes.size <=> b.scopes.size }
+    consumer = sp.consumers.first
+    user = User.find_by(uid: ldap_user.uid)
+    token = consumer.tokens.create!(grant: 'authorization_code', user: user)
+    redirect_uri = consumer.redirect_uris.first.uri
+    state = 'abcABC'
+    token.set_as_authorization_code(sp.scopes.map(&:name), state, redirect_uri)
+    params = {
+      grant_type: 'authorization_code',
+      client_id: consumer.client_id_key,
+      client_secret: consumer.client_secret,
+      redirect_uri: redirect_uri,
+      code: token.code,
+      scope: sp.scopes.map{ |s| s.name }.join(' '),
+      state: state
+    }
+    post(oauth2_token_path(sp.id), params: params)
+    assert_response(:success)
+    json = JSON.parse(response.body)
+    assert_equal('success', json['status'])
+    assert_not_nil(json['access_token'])
+    assert_not_nil(json['refresh_token'])
+    assert_equal('Bearer', json['token_type'])
+    assert_kind_of(Integer, json['expires_in'])
+    assert_equal(sp.scopes.map{ |s| s.name }.join(' '), json['scope'])
+    assert_equal('abcABC', json['state'])
+  end
 end
 
