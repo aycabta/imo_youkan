6aabb32812aac468d98a7a979176778e61004a6c
Code Ass <aycabta@gmail.com>
2017-05-17 11:08:10 +0900

    Add Flash message around log-in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
     if !ldap_user.nil? && ActiveLdap::UserPassword.valid?(params['password'], ldap_user.userPassword)
       @user = User.find_or_create_by_auth(ldap_user)
       session[:user_id] = @user.id
+      flash[:login_alert] = 'logged in'
       if session[:continued_url]
         url = session[:continued_url]
         session.delete(:continued_url)
@@ -13,6 +14,7 @@ class SessionsController < ApplicationController
         redirect_to(root_path)
       end
     else
+      flash[:login_alert] = 'username or password is wrong'
       redirect_to(root_path)
     end
   end
