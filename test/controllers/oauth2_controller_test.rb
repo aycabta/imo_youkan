0f30c7e6cf830a4f3dc77774976efd3a5f65b613
Code Ass <aycabta@gmail.com>
2017-06-25 13:58:43 +0900

    Add test for /oauth2/introspect with revoked token

--- test/controllers/oauth2_controller_test.rb
+++ test/controllers/oauth2_controller_test.rb
@@ -198,6 +198,33 @@ class OAuth2ControllerTest < ActionDispatch::IntegrationTest
     assert_equal(state, json['state'])
   end
 
+  test 'should fail /oauth2/introspect with revoked token' do
+    ldap_user = sign_in_as(:great_user)
+    sp = ServiceProvider.all.max{ |a, b| a.scopes.size <=> b.scopes.size }
+    consumer = sp.consumers.first
+    user = User.find_by(uid: ldap_user.uid)
+    token = consumer.tokens.create
+    state = 'abcABC'
+    token.set_as_client_credentials
+    params = {
+      client_id: consumer.client_id_key,
+      client_secret: consumer.client_secret,
+      token: token.access_token,
+      state: state
+    }
+    post(oauth2_revoke_path(sp), params: params)
+    params = {
+      client_id: consumer.client_id_key,
+      client_secret: consumer.client_secret,
+      token: token.access_token,
+      state: state
+    }
+    post(oauth2_introspect_path(sp), params: params)
+    json = JSON.parse(response.body)
+    assert_equal(false, json['active'])
+    assert_equal(state, json['state'])
+  end
+
   test 'should fail /oauth2/revoke with invalid access_token' do
     ldap_user = sign_in_as(:great_user)
     sp = ServiceProvider.all.max{ |a, b| a.scopes.size <=> b.scopes.size }
